a58f5e148d7aa63aee012a3c3b4d16ddc8f6c8a4
yoligAW27 <yohanagezahagn@gmail.com>
2025-11-15 03:42:23 -0800

    Add files via upload

---
+++ controlflow/weather_advice.py
@@ -0,0 +1,10 @@
+weather=input("whats today weatehr looks like? (sunny/rainy/cold):")
+if weather=="sunny":
+    print("wear t-shirt and sunglasses")
+elif weather=="rainy":
+    print("dont forget umbrella and bots")
+elif weather=="cold":
+    print("make sure to wear warm clothes")
+else:
+    print("sorry, i dont have recommandation for this weather")
+    
