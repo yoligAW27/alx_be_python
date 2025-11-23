5f984e137e946471093759daac7f02f8e78f9c40
yoligAW27 <yohanagezahagn@gmail.com>
2025-11-23 01:37:05 -0800

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1:float, num2:float, operation:str):
+    match operation:
+        case "add":
+            return num1+num2
+        case "subtract":
+            return num1-num2
+        case "multiply":
+            return num1*num2
+        case "divide":
+            if num2==0:
+                return "can not divide by zero"
+            else:
+                return num1/num2
+        case _:
+            return "Invalid operation"    
+
+
+    
