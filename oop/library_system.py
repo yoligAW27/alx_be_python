196b33395d0ff55586e08d8f509a11ba9d978063
yoligAW27 <yohanagezahagn@gmail.com>
2025-12-06 05:10:16 -0800

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,13 +9,13 @@ class EBook(Book):
      super().__init__(title, author)
      self.file_size = file_size
     def __str__(self): 
-       return f"EBook: {self.title} by {self.author}, File_size: {self.file_size}"
+       return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 class PrintBook(Book):
     def __init__(self, title:str, author:str, page_count:int):
        super().__init__(title, author)
        self.page_count=page_count
     def __str__(self):
-       return f"PrintBook: {self.title} by {self.author}, page_count: {self.page_count}"
+       return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class Library:
     def __init__(self):
         self.books = []  
@@ -29,5 +29,6 @@ class Library:
     
          
           
+
 
 
