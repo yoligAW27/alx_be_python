494934c0f93e8ad5fcc14e957988a6e7fea3635a
yoligAW27 <yohanagezahagn@gmail.com>
2025-12-06 04:59:07 -0800

    Add files via upload

---
+++ oop/library_system.py
@@ -0,0 +1,31 @@
+class Book:
+    def __init__(self, title:str, author:str):
+          self.title=title
+          self.author=author
+    def __str__(self):
+     return f"Book: {self.title} {self.author}"
+class EBook(Book):
+    def __init__(self, title:str, author:str, file_size:int):
+     super().__init__(title, author)
+     self.file_size = file_size
+    def __str__(self): 
+       return f"EBook:{self.title} by {self.author}, File_size: {self.file_size}"
+class PrintBook(Book):
+    def __init__(self, title:str, author:str, page_count:int):
+       super().__init__(title, author)
+       self.page_count=page_count
+    def __str__(self):
+       return f"PrintBook:{self.title} by {self.author}, page_count: {self.page_count}"
+class Library:
+    def __init__(self):
+        self.books = []  
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)  
+    
+         
+          
